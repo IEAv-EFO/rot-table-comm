b24bbfd054fd97f0d6fec3978dba64e8dd020841
Gabriel Nunes <nunesgfs@gmail.com>
2024-03-05 20:16:20 -0300

    replace strings by fstring

--- rot_table.py
+++ rot_table.py
@@ -1,28 +1,23 @@
-
 import requests
 
 api_addr = "http://127.0.0.1:8088/api/"
 str_json_request = ".json?"
 
 def send_command(command="getConfig"):
-    out = requests.get(api_addr + command + str_json_request)
+    out = requests.get(f"{api_addr}{command}{str_json_request}")
     print(out.json())
 
 def set_pos(pitch, yaw, command="executaPosicionamento"):
-    out = requests.get(api_addr + command + str_json_request + "posAzimute=" + str(yaw) + "&posTilt=" + str(pitch))
+    out = requests.get(f"{api_addr}{command}{str_json_request}posAzimute={yaw}&posTilt={pitch}")
     print(out.json())
 
 def set_vel(pitch, yaw, command="executaJog"):
     if pitch == 0 & yaw == 0:
         stop()
-    out = requests.get(api_addr + command + str_json_request + "velAzimute=" + str(yaw) + "&velTilt=" + str(pitch))
+    out = requests.get(f"{api_addr}{command}{str_json_request}velAzimute={yaw}&velTilt={pitch}")
     print(out.json())
 
 def stop():
     out = requests.get("http://127.0.0.1:8088/api/executaParar.json?")
     print(out.json())
 
-
-send_command()
-set_pos(0, 0)
-set_vel(0, 0)
